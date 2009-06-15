148404a080746d74b22e0728c00451290cdf6f21
aanjhan <aanjhan@807c5c5c-31e3-11de-ba44-d58fe8a9ce33>
2009-06-15 15:57:06 +0000

    Added License headers according to RTEMS coding conventions

--- libmmu/libmmu.h
+++ libmmu/libmmu.h
@@ -1,5 +1,15 @@
 #ifndef _RTEMS_LIBMMU_H
 #define _RTEMS_LIBMMU_H
+/*
+*  COPYRIGHT (c) 1989-2007.
+*  On-Line Applications Research Corporation (OAR).
+*
+*  The license and distribution terms for this file may be
+*  found in the file LICENSE in this distribution or at
+*  http://www.rtems.com/license/LICENSE.
+*
+*  $Id$
+*/
 
 #include <inttypes.h>
 
