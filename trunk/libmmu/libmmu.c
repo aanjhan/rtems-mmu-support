acfaf0f30f5a7e0829e88f6811345cc67ca332f6
aanjhan <aanjhan@807c5c5c-31e3-11de-ba44-d58fe8a9ce33>
2009-06-15 15:57:06 +0000

    Added License headers according to RTEMS coding conventions

--- trunk/libmmu/libmmu.c
+++ trunk/libmmu/libmmu.c
@@ -1,23 +1,38 @@
+/*
+*  COPYRIGHT (c) 1989-2007.
+*  On-Line Applications Research Corporation (OAR).
+*
+*  The license and distribution terms for this file may be
+*  found in the file LICENSE in this distribution or at
+*  http://www.rtems.com/license/LICENSE.
+*
+*  $Id$
+*/
+
 #include <stdio.h>
 #include <string.h>
-
 #include "libmmu.h"
 
 
-
-
+/*****************************************************
+* Initialisation of libmmu things goes here          *
+*****************************************************/
 void
 rtems_libmmu_alut_init(void)
 {
   /* Initialisation code goes here */
 }
     
-
+/*****************************************************
+* Returns the access attribute for a particular addr *
+* location                                           *
+*****************************************************/
 int
 rtems_libmmu_get_access_attribute(uint32_t addr)
 {
   return rtems_libmmu_alut[0].access_attrib;
 }
+
 
 int
 rtems_libmmu_list_alut_access(void)
