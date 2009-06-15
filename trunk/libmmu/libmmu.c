f7bee146d8a43dddc4a65354701d9ec904845d26
aanjhan <aanjhan@807c5c5c-31e3-11de-ba44-d58fe8a9ce33>
2009-06-15 08:51:01 +0000

    Added Access Protection Mode definitions, added init alut function stub

--- trunk/libmmu/libmmu.c
+++ trunk/libmmu/libmmu.c
@@ -3,14 +3,30 @@
 
 #include "libmmu.h"
 
+rtems_libmmu_alut rtems_libmmu_alut_entry[RTEMS_LIBMMU_ALUT_SIZE];
+
+
+static void
+rtems_libmmu_alut_init(void)
+{
+  static const rtems_libmmu_alut[] = 
+  {
+    0x00000000, 4096, 0;
+    NULL;
+    NULL;
+    NULL;
+  };
+}
+    
+
 int
 rtems_libmmu_get_access_attribute(uint32_t addr)
 {
-  return 1;
+  return 0;
 }
 
 int
 rtems_libmmu_list_alut_access(void)
 {
-  return 1;
+  return 0;
 }
--- trunk/libmmu/libmmu.h
+++ trunk/libmmu/libmmu.h
@@ -3,6 +3,26 @@
 
 #include <inttypes.h>
 
+
+#define RTEMS_LIBMMU_ALUT_SIZE 32
+
+
+/* Access definition macros below */
+#define RTEMS_LIBMMU_ACCESS_PROT_NONE      0
+#define RTEMS_LIBMMU_ACCESS_PROT_READ      1
+#define RTEMS_LIBMMU_ACCESS_PROT_WRITE     2
+#define RTEMS_LIBMMU_ACCESS_PROT_EXECUTE   3
+
+
+typedef struct
+{
+  uint32_t start_addr;
+  uint32_t block_size;
+  unsigned char access_attrib;
+} rtems_libmmu_alut_entry;
+
+
+
 extern int
 rtems_libmmu_get_access_attribute(uint32_t addr);
 
