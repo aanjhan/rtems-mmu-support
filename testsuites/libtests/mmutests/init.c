4054b592a78f2a8274a734b7bc325260bbe65e6f
aanjhan <aanjhan@807c5c5c-31e3-11de-ba44-d58fe8a9ce33>
2009-07-04 14:06:47 +0000

    failed inclusion of libmmu.h

--- testsuites/libtests/mmutests/init.c
+++ testsuites/libtests/mmutests/init.c
@@ -24,13 +24,37 @@
 #include "system.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <rtems/cpukit/libmmu/libmmu.h>
 
 rtems_task Init(
   rtems_task_argument ignored
 )
 {
-  printf( "\n\n*** HELLO MMU TEST ***\n" );
-  printf( "Hello MMU\n" );
-  printf( "*** END OF HELLO MMU TEST ***\n" );
-  exit( 0 );
+  int access;
+  rtems_libmmu_alut_entry Entry;
+  rtems_libmmu_alut* pAlut;
+  pAlut = rtems_libmmu_alut_create(3);
+  printf("ALUT created\n");
+  Entry.start_addr = 0x00000000;
+  Entry.block_size = 0x00000FFF;
+  Entry.access_attrib = 1;
+  rtems_libmmu_alut_add_entry(pAlut,&Entry);
+  printf("1 Entry added\n");
+  
+  Entry.start_addr = 0x02000000;
+  Entry.block_size = 0x00000FFF;
+  Entry.access_attrib = 3;
+  rtems_libmmu_alut_add_entry(pAlut,&Entry);
+  printf("1 Entry added\n");
+  
+  Entry.start_addr = 0x00008000;
+  Entry.block_size = 0x000007FF;
+  Entry.access_attrib = 2;
+  rtems_libmmu_alut_add_entry(pAlut,&Entry);
+  printf("1 Entry added\n");
+  
+  printf("Searching for access attrbute for address 0x00008111...\n");
+  access = rtems_libmmu_get_access_attribute(pAlut, 0x00008111);
+  printf("Access atribute is %d\n", access);
+  return 0;
 }
